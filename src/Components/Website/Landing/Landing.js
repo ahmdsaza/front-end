bd8ad51956757f5ba7bf74c31fdacde02e67e01a
ahmdsaza <ahmdsaza@gmail.com>
2024-10-29 18:49:44 +0300

    Improve landing design

--- src/Components/Website/Landing/Landing.js
+++ src/Components/Website/Landing/Landing.js
@@ -1,25 +1,42 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import Carousel from "react-bootstrap/Carousel";
+import "./landing.css";
+import banner1 from "../../../Assets/2.jpg";
 
 export default function Landing() {
   return (
-    <div className="d-flex align-items-center justify-content-between flex-wrap hand">
-      <Container>
-        <div className="col-lg-5 col-md-8 col-12 text-md-start text-center">
-          <h1 className="display-2 fw-bold">T-Shirts!</h1>
-          <h5 style={{ color: "black" }} className="fw-normal">
-            New T-shirts with different colors and sizes !
-          </h5>
-
-          <Link
-            to="/categories"
-            className="btn btn-primary mt-3 py-3 px-4 fw-bold text-light"
-          >
-            Shop Now
-          </Link>
-        </div>
-      </Container>
-    </div>
+    <Container className="my-4">
+      <Carousel>
+        <Carousel.Item>
+          <NavLink to="/categories/1">
+            <img className="d-block w-100" src={banner1} alt="First slide" />
+            <Carousel.Caption>
+              {/* <h5>First slide label</h5>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+            </Carousel.Caption>
+          </NavLink>
+        </Carousel.Item>
+        <Carousel.Item>
+          <NavLink to="/categories/2">
+            <img className="d-block w-100" src={banner1} alt="First slide" />
+            <Carousel.Caption>
+              {/* <h5>First slide label</h5>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+            </Carousel.Caption>
+          </NavLink>
+        </Carousel.Item>
+        <Carousel.Item>
+          <NavLink to="/categories/3">
+            <img className="d-block w-100" src={banner1} alt="First slide" />
+            <Carousel.Caption>
+              {/* <h5>First slide label</h5>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+            </Carousel.Caption>
+          </NavLink>
+        </Carousel.Item>
+      </Carousel>
+    </Container>
   );
 }
